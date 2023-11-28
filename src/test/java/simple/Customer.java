5847fa0051146b22b77ae6b893f4812b3cc3c90d
SHRU <SHRU@DESKTOP-56HO2UA>
2023-11-28 16:07:56 +0530

    new package

---
+++ src/test/java/simple/Customer.java
@@ -0,0 +1,18 @@
+package simple;
+
+import org.testng.annotations.Test;
+
+public class Customer {
+	@Test
+	public void simple1()
+	{
+		System.out.println("preactice implementation");
+	}
+	@Test
+	public void simplePro()
+	{
+		System.out.println("preactice implementation  m");
+	}
+
+
+}
