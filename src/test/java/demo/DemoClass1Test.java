7a5da53e23d09f75184f48fcf263b6c58fdbb952
susendran <susee.jt@gmail.com>
2023-11-28 16:00:13 +0530

    master frame

---
+++ src/test/java/demo/DemoClass1Test.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoClass1Test {
+	@Test
+	public void demo1()
+	{
+		System.out.println("Hi demo1");
+	}
+
+}
---
+++ src/test/java/demo/DemoClass2Test.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoClass2Test {
+	@Test
+	public void demo2()
+	{
+		System.out.println("Hi demo2");
+	}
+
+}
